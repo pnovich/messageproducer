dd459df3f490c595c0ed9fd1de470f65ea8c0572
PavelVasianovych <pppp_84@ukr.net>
2023-11-28 00:13:59 +0200

    commented test consumer

--- src/main/java/com/example/messageproducer/MessageReceiver.java
+++ src/main/java/com/example/messageproducer/MessageReceiver.java
@@ -6,23 +6,23 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MessageReceiver {
 
-    @Autowired
-    Queue queue;
-
-    @Autowired
-    TicketService ticketService;
-
-    @PostConstruct
-    public void initMethod() {
-        System.out.println("listener initialized");
-    }
-
-    @RabbitListener(queues = "#{queue.getName()}")
-    public void handleMessage(String message) {
-        System.out.println("message received");
-        ticketService.saveTicket(message);
-    }
+//    @Autowired
+//    Queue queue;
+//
+//    @Autowired
+//    TicketService ticketService;
+//
+//    @PostConstruct
+//    public void initMethod() {
+//        System.out.println("listener initialized");
+//    }
+//
+//    @RabbitListener(queues = "#{queue.getName()}")
+//    public void handleMessage(String message) {
+//        System.out.println("message received");
+//        ticketService.saveTicket(message);
+//    }
 }
