b72a08bdde00bcd0f14a143bbd71577b5ba48c07
PavelVasianovych <pppp_84@ukr.net>
2023-11-29 11:19:28 +0200

    implemented kafka topic

--- src/main/java/com/example/messageproducer/TicketController.java
+++ src/main/java/com/example/messageproducer/TicketController.java
@@ -1,7 +1,9 @@
 package com.example.messageproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,6 +13,11 @@ public class TicketController {
     @Autowired
     TicketService ticketService;
 
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+
+    String topic = "topic1";
+
     @GetMapping("/")
     public String getDefaultString() {
         return "default string";
@@ -19,5 +26,13 @@ public class TicketController {
     @GetMapping("/tickets")
     public List<Ticket> getAllTickets() {
         return ticketService.getAllTickets();
+    }
+
+    @GetMapping("/send/{message}")
+    public String sendMessage(@PathVariable("message") String message) {
+//        rabbitTemplate.convertAndSend(binding.getExchange(), binding.getRoutingKey(), message);
+        kafkaTemplate.send(topic, message);
+        System.out.println("message sent");
+        return "sent";
     }
 }
